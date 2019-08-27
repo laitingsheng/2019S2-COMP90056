06921a2eb1af063fe77bdff45982556c27f630de
Tinson Lai <laitinson@gmail.com>
2019-08-27 23:39:34 +1000

    Fix but broken

--- Tutorial/src/test/java/dev/tinson/unimelb/comp90056/java/week5/Week5Test.java
+++ Tutorial/src/test/java/dev/tinson/unimelb/comp90056/java/week5/Week5Test.java
@@ -12,6 +12,8 @@ public final class Week5Test {
     public void testAMSCounter() {
         DistinctCounter<Integer> counter = new AMSDistinctCounter<>(100, 0.1);
         double d = new Random().ints(100, 0, 100).peek(counter::add).distinct().count(), cd = counter.distinct();
-        assertTrue(cd >= d * 0.9 && cd <= cd * 1.1);
+        System.out.println(d);
+        System.out.println(cd);
+        assertTrue(cd >= d * 0.9 && cd <= d * 1.1);
     }
 }
