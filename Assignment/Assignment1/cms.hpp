6d3d594ce12a6ff24954a36f495ec74c754e91da
Tinson Lai <laitinson@gmail.com>
2019-09-02 12:50:55 +1000

    Minor update

--- Assignment/Assignment1/cms.hpp
+++ Assignment/Assignment1/cms.hpp
@@ -1,13 +1,22 @@
 #ifndef _CMS_HPP_
 #define _CMS_HPP_
 
+#include <cmath>
+
+#include <omp.h>
+
 template<typename T>
 struct cms_default
 {
     explicit cms_default(double epsilon, double delta)
-    {}
+    {
+        w = ceil(2 / epsilon / epsilon);
+        d = ceil(log(1 / delta));
+        hashes = new hash[d];
+    }
 private:
     unsigned long const w, d;
+    hash const * const hashes;
 }
 
 #endif
