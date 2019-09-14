b46b43ae9fccd21d366e73a37dedb914be9ba43e
Tinson Lai <laitinson@gmail.com>
2019-09-15 06:21:39 +1000

    Untested stream

--- Assignment/Assignment1/stream.hpp
+++ Assignment/Assignment1/stream.hpp
@@ -10,13 +10,14 @@ struct stream
     using UpdateType = std::conditional_t<force_positive_update, uint16_t, int16_t>;
     using Counter = std::unordered_map<Type, int32_t>;
 
-    static std::uniform_int_distribution<uint8_t> rb;
+    std::uniform_int_distribution<uint8_t> rb;
 
     uint8_t const min_repeat, max_repeat;
     std::uniform_int_distribution<uint16_t> rs;
     std::uniform_int_distribution<UpdateType> rf;
 
-    std::unordered_map<Type, std::pair<int32_t, uint8_t>> record;
+    std::unordered_map<Type, std::pair<int32_t, uint16_t>> record;
+    std::vector<Type> distinct;
     int64_t F1;
 
     virtual Type generate() = 0;
@@ -29,23 +30,28 @@ protected:
            UpdateType min_update,
            UpdateType max_update) : g(std::random_device()()),
                                     rf(min_update, max_update),
-                                    rs(0, num_distinct),
+                                    rs(0, num_distinct - 1),
                                     max_repeat(max_repeat),
                                     min_repeat(min_repeat),
+                                    distinct(num_distinct),
                                     record(num_distinct)
     {
-        while (record.size() < num_distinct)
+        size_t i = 0;
+        while (i < num_distinct)
         {
             auto item = generate();
             if (!record.count(item))
+            {
+                distinct[i++] = item;
                 record[std::move(item)] = {};
+            }
         }
     }
 public:
     // prevent unintentional copy
     stream(stream const &) = delete;
 
-    virtual Counter counter() final
+    virtual Counter counter() const final
     {
         Counter counter;
         for (auto const & [k, v] : record)
@@ -57,31 +63,40 @@ public:
     {
         while (true)
         {
-            Type item = generate();
-            auto & r = repeats[item];
-            if (r < max_repeat)
+            Type const & item = distinct[rs(g)];
+            auto & [f, r] = record[item];
+            if (r == max_repeat)
+                continue;
+            int32_t freq;
+            if constexpr (force_positive_update)
+                freq = rf(g);
+            else
             {
-                ++r;
-                if constexpr (force_positive_update)
-                {
-                    int32_t f = rf(g);
-                    counter[item] += f;
-                    F1 += f;
-                    return { item, f };
-                }
+                if (r == max_repeat - 1)
+                    if (f < 0)
+                        freq = -f + rb(g);
+                    else
+                        freq = rb(g);
                 else
-                {}
+                    freq = rf(g);
             }
+            f += freq;
+            F1 += frq;
+            ++r;
+            return { item, freq }
         }
         throw "exceeding stream limit";
     }
 
     virtual bool operator!() const final
     {
-        for (auto const & [_, v] : repeats)
-            if (v < min_repeat)
+        for (auto const & [_, v] : record)
+        {
+            auto const & [f, r] = v;
+            if (r < min_repeat || f < 0)
                 return false;
-        return counter.size() == num_distinct;
+        }
+        return true;
     }
 
     virtual operator int64_t() const final
@@ -89,5 +104,25 @@ public:
         return F1;
     }
 };
+
+template<typename IntType, bool force_positive_update = true>
+class int_stream : public stream<IntType, force_positive_update>
+{
+    std::uniform_int_distribution<IntType> ri;
+
+    virtual IntType generate() override
+    {
+        return ri(this->g);
+    }
+public:
+    int_stream(uint16_t num_distinct,
+               uint8_t min_repeat,
+               uint8_t max_repeat,
+               UpdateType min_update,
+               UpdateType max_update,
+               IntType min_value,
+               IntType max_value) : ri(min_value, max_value),
+                                    stream(num_distinct, min_repeat, max_repeat, min_update, max_update) {}
+}
 
 #endif
